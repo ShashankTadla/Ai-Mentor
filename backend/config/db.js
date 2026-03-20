4803fb6a2c623cb330803fbf47d53f25180942c9
Mukesh Choudhary <mukeshchoudhary33333@gmail.com>
2026-03-20 20:36:18 +0530

    Fixes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,17 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: process.env.DB_PORT,
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.NEON_DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // required for Neon
+    },
+  },
+});
 
 const connectDB = async () => {
   try {
